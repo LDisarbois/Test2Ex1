9c2919bdc72c422d08a1b9a03228bd968859278f
loic disarbois <loic.disarbois@gmail.com>
2019-02-28 15:50:15 +0100

    1

---
+++ Exercice1Test2/src/Ex1/RomanToNumeral.java
@@ -0,0 +1,20 @@
+package Ex1;
+
+import java.util.ArrayList;
+
+public class RomanToNumeral {
+
+	
+	public int getNumeral(ArrayList<String> a) {
+		int b;
+		
+		for(int i=0;i<a.size();i++) {
+			if(a.indexOf())
+		}
+		
+		return b;
+		
+		
+	}
+	
+}
