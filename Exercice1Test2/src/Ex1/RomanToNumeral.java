343de27306c2dcb10a81f724b51336e384e01699
loic disarbois <loic.disarbois@gmail.com>
2019-02-28 15:55:27 +0100

    2

--- Exercice1Test2/src/Ex1/RomanToNumeral.java
+++ Exercice1Test2/src/Ex1/RomanToNumeral.java
@@ -6,15 +6,37 @@ public class RomanToNumeral {
 
 	
 	public int getNumeral(ArrayList<String> a) {
-		int b;
+		int b=0;
 		
 		for(int i=0;i<a.size();i++) {
-			if(a.indexOf())
+			if(a.get(i).toString() == "I") {
+				b = b + 1;
+			}else {
+				if(a.get(i).toString() == "V") {
+					b=b+5;
+				}else {
+					if(a.get(i).toString() == "X") {
+						b=b+10;
+					}else {
+						if(a.get(i).toString() == "L") {
+							b=b+50;
+						}else {
+							if(a.get(i).toString() == "C") {
+								b=b+100;
+							}else {
+								if(a.get(i).toString() == "D") {
+									b=b+500;
+								}else {
+									if(a.get(i).toString() == "M") {
+										b=b+1000;
+									}
+							}
+						}
+					}
+				}
+			}
 		}
-		
-		return b;
-		
-		
+		}
+return b;
 	}
-	
 }
